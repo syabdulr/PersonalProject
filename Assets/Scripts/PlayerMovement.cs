e7f030834c6e3583a0488b1ec88d61fa5ce58211
Abdul Syed <syabdulr@sheridancollege.ca>
2023-03-11 12:42:31 -0500

    Implemented a tutorial area
    Camera now sticks to the car and the car moves around the tutorial area

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
